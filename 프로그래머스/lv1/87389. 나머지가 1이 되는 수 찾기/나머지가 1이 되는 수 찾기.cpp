d82351ab7ef56aca2878e57523bd2c45ca7611da
이문빈 <3412mb@gmail.com>
2023-05-05 04:28:10 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    for (int i = 2; ; i++) {
+        if ((n - 1) % i == 0) return i;
+    }
+    return -1;
+}
