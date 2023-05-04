b5f23a5356eb352b22cc5788fc76f98d27d71015
이문빈 <3412mb@gmail.com>
2023-05-04 19:33:24 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int i = 1;
+    for (; i * i < n; i++) {
+        if (n % i == 0) {
+            answer += i + n / i;
+        }
+    }
+    if (i * i == n) answer += i;
+    return answer;
+}
