d5176c3828b8b0ea02f9f395a91065ee2b262e75
이문빈 <3412mb@gmail.com>
2023-05-07 20:04:34 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.33 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,18 @@
+import re
+
+def solution(new_id):
+    answer = new_id.lower()
+    answer = re.sub('[^a-z0-9-_\.]', '', answer)
+    answer = re.sub('\.{2,}', '.', answer)
+    answer = re.sub('^\.', '', answer)
+    answer = re.sub('\.$', '', answer)
+    
+    if len(answer) == 0:
+        answer = 'aaa'
+    elif len(answer) >= 16:
+        answer = answer[:15]
+        answer = re.sub('\.$', '', answer)
+    else:
+        while len(answer) <= 2:
+            answer += answer[-1]
+    return answer
