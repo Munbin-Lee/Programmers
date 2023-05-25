6108e23cd9bde48c51fa0fb2dd695a1430873717
이문빈 <3412mb@gmail.com>
2023-05-25 16:59:49 +0900

    [level 2] Title: 전화번호 목록, Time: 153.81 ms, Memory: 177 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+#include <array>
+#include <algorithm>
+
+using namespace std;
+
+struct Node {
+    bool isTerminal;
+    array<Node*, 10> child;
+};
+
+int nodeCnt;
+Node nodes[2000099];
+
+Node* createNode() {
+    auto pNode = &nodes[nodeCnt++];
+    pNode->isTerminal = false;
+    pNode->child.fill(nullptr);
+    return pNode;
+}
+
+Node* root = createNode();
+
+bool hasPrefix(string &s) {
+    auto pNode = root;
+    for (char ch : s) {
+        int idx = ch - '0';
+        if (!pNode->child[idx]) {
+            pNode->child[idx] = createNode();
+        } else if (pNode->child[idx]->isTerminal) return true;
+        pNode = pNode->child[idx];
+    }
+    pNode->isTerminal = true;
+    return false;
+}
+
+bool solution(vector<string> phone_book) {
+    sort(phone_book.begin(), phone_book.end());
+    for (string &s : phone_book) {
+        if (hasPrefix(s)) return false;
+    }
+    return true;
+}
