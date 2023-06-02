a87febfe57bbc541063adf43a4cc121d4c89e6ef
이문빈 <3412mb@gmail.com>
2023-06-02 13:25:25 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    for (char &ch : my_string) {
+        if (isupper(ch)) ch = tolower(ch);
+        else ch = toupper(ch);
+    }
+    return my_string;
+}
