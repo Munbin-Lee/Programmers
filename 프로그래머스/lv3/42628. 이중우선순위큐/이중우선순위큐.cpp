f5837de635dfce5bf119e1e80d0cbf65d60b2d8d
이문빈 <3412mb@gmail.com>
2023-10-08 18:43:33 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <set>
+
+using namespace std;
+
+set<int> st;
+
+void processOperation(string& operation) {
+    istringstream sin(operation);
+    char cmd; int num;
+    sin >> cmd >> num;
+    
+    if (cmd == 'I') {
+        st.emplace(num);
+    } else if (cmd == 'D' && num == 1) {
+        if (st.empty()) return;
+        auto it = prev(st.end());
+        st.erase(it);
+    } else if (cmd == 'D' && num == -1) {
+        if (st.empty()) return;
+        auto it = st.begin();
+        st.erase(it);
+    }
+}
+
+vector<int> solution(vector<string> operations) {
+    for (string& operation : operations) {
+        processOperation(operation);
+    }
+    
+    vector<int> answer(2);
+    if (st.empty()) return answer;
+    
+    answer[0] = *st.rbegin();
+    answer[1] = *st.begin();
+    return answer;
+}
