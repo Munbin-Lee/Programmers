2be406b9610b2991f9e4eb5e480ed098acef0f23
이문빈 <3412mb@gmail.com>
2023-10-12 15:23:25 +0900

    [level 2] Title: 디펜스 게임, Time: 0.16 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -1,14 +1,31 @@
-import heapq
-
-def solution(n, k, enemies):
-    # k번째 적까지 힙에 넣음
-    q = enemies[:k]
-    heapq.heapify(q)
+def solution(n, k, enemy):
     
-    # 나머지 적들을 순회하면서 가장 적은 적은 pop
-    for round, enemy in enumerate(enemies[k:]):
-        n -= heapq.heappushpop(q, enemy)
-        if n < 0:
-            return round + k
+    left = 0
+    right = len(enemy)
     
-    return len(enemies)
+    while True:
+        mid = (left + right)//2
+        
+        if left>right:
+            break
+            
+        now_enemy = sorted(enemy[:mid])
+        
+        for _ in range(k):
+            if now_enemy:
+                now_enemy.pop()
+                
+            else:
+                break
+            
+        temp = sum(now_enemy)
+        
+        if temp > n :
+            right = mid-1
+            
+        elif temp < n :
+            left = mid+1
+            
+        else:
+            return end
+    return mid
