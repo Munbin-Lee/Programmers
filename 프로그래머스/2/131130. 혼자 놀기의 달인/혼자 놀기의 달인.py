2c4ed053ca4c6a9a3277eddb931986754db937e0
이문빈 <3412mb@gmail.com>
2023-10-16 14:40:47 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 23.30 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -0,0 +1,19 @@
+def solution(cards):
+    def dfs(cur, visited):
+        if visited & (1 << cur): return visited
+        visited |= (1 << cur)
+        next = cards[cur] - 1
+        return dfs(next, visited)
+    
+    def bit_count(x):
+        return bin(x).count('1')
+    
+    answer = 0
+    for i in range(len(cards)):
+        visited = dfs(i, 0)
+        a = bit_count(visited)
+        for j in range(len(cards)):
+            visited2 = dfs(j, visited)
+            b = bit_count(visited2) - a
+            answer = max(answer, a * b)
+    return answer
