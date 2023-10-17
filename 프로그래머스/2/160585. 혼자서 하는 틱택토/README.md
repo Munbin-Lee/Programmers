e6c4a25cd964794893b5a44e1e35a7f87d83ed34
이문빈 <3412mb@gmail.com>
2023-10-17 23:50:56 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 4.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/README.md
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/README.md
@@ -1,10 +1,10 @@
 # [level 2] 혼자서 하는 틱택토 - 160585 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160585?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160585?language=python3#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 1201.59 ms
+메모리: 10.3 MB, 시간: 4.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 23:23:22
+2023년 10월 2일 23:50:54
 
 ### 문제 설명
 
--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -1,30 +1,62 @@
-def swap(player):
-    return player == 'O' and 'X' or 'O'
+from copy import deepcopy
 
-def end(board):
-    for line in board:
-        if line == 'OOO' or line == 'XXX': return True
-    
-    for i in range(3):
-        if board[0][i] == board[1][i] == board[2][i] != '.': return True
-        
-    if board[0][0] == board[1][1] == board[2][2] != '.': return True
+check = ["O", "X"]
+result = 0
 
-    if board[0][2] == board[1][1] == board[2][0] != '.': return True
-    
+
+def check_valid(visited):
+    if all(-1 not in element for element in visited):
+        global result
+        result = 1
+        return True
     return False
 
+
+def pre_win(visited, cnt, i, j):
+    global result
+    if all(cnt % 2 == k for k in visited[i]):
+        if check_valid(visited):
+            result = 1
+        return 0
+    if cnt % 2 == visited[0][j] and cnt % 2 == visited[1][j] and cnt % 2 == visited[2][j]:
+        if check_valid(visited):
+            result = 1
+        return 0
+
+    if all(cnt % 2 == visited[k][k] for k in range(3)):
+        if check_valid(visited):
+            result = 1
+        return 0
+    if all(cnt % 2 == visited[2 - k][k] for k in range(3)):
+        if check_valid(visited):
+            result = 1
+        return 0
+    return 1
+
+
+def dfs(boards, visited, cnt):
+    if check_valid(visited):
+        return
+    for i in range(3):
+        for j in range(3):
+            if boards[i][j] == check[cnt % 2] and visited[i][j] == -1:
+                new_visited = deepcopy(visited)
+                new_visited[i][j] = cnt % 2
+                if pre_win(new_visited, cnt, i, j) == 1:
+                    dfs(boards, new_visited, cnt + 1)
+
+
+
 def solution(board):
-    def dfs(player, newBoard):
-        if (board == newBoard): return True
-        if (end(newBoard)): return False
-        for i, line in enumerate(newBoard):
-            for j, cell in enumerate(line):
-                if cell != '.': continue
-                temp = newBoard[i]
-                newBoard[i] = newBoard[i][:j] + player + newBoard[i][j+1:]
-                if dfs(swap(player), newBoard): return True
-                newBoard[i] = temp
-        return False
-    emptyBoard = ['.' * 3 for _ in range(3)]
-    return int(dfs('O', emptyBoard))
+    visited = [[-1] * len(board) for i in range(len(board))]
+    boards = []
+    for element in board:
+        boards.append(list(element))
+
+    for i in range(3):
+        for j in range(3):
+            if boards[i][j] == ".":
+                visited[i][j] = -2
+
+    dfs(boards, visited, 0)
+    return result
