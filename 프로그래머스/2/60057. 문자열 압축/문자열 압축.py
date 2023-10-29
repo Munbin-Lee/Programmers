d92d8ddf9162537a9b8ab2f4a84ed4a05940bf68
이문빈 <3412mb@gmail.com>
2023-10-30 02:53:40 +0900

    [level 2] Title: 문자열 압축, Time: 0.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,30 +1,12 @@
+import re
+
+def replace(m):
+    a, b = m.groups()
+    cnt = len(b) // len(a)
+    return (cnt and str(cnt + 1) or '') + a
+
 def solution(s):
-    len_total_s = len(s)
-    answer = int(1e9)
-    
-    if len_total_s == 1:
-        return 1
-    
-    for len_s in range(1,len_total_s):
-        stack = []     
-        temp_list_for_string = []
-        
-        for char_idx in range(0,len_total_s,len_s):
-            now = s[char_idx:char_idx+len_s]
-
-            if len(stack) > 0 and now == stack[-1]:
-                temp_list_for_string[-1][1] += 1
-                continue
-                
-            temp_list_for_string.append([now,1])
-            stack.append(now)
-
-        temp_str = ""
-        for key, value in temp_list_for_string:
-            if value != 1:
-                temp_str += str(value)
-
-            temp_str += key
-        answer = min(answer,len(temp_str))
-        
+    answer = len(s)
+    for i in range(1, len(s) // 2 + 1):
+        answer = min(answer, len(re.sub(r"([a-z]{%d})(\1*)" %i, replace, s)))
     return answer
