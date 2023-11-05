131cd35db5c0e9687fbcbdaf0c8d74b8ad84827e
이문빈 <3412mb@gmail.com>
2023-11-05 10:45:08 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,15 +1,21 @@
-from collections import defaultdict
-
 def solution(weights):
-    answer = 0
-    counts = defaultdict(int)
-    for weight in weights:
-        answer += counts[weight]
-        answer += counts[weight * 2]
-        answer += weight % 2 == 0 and counts[weight // 2] or 0
-        answer += weight % 2 == 0 and counts[weight // 2 * 3] or 0
-        answer += weight % 3 == 0 and counts[weight // 3 * 2] or 0
-        answer += weight % 3 == 0 and counts[weight // 3 * 4] or 0
-        answer += weight % 4 == 0 and counts[weight // 4 * 3] or 0
-        counts[weight] += 1
-    return answer
+    weight_dict = {}
+    for w in weights:
+        # weight의 개수 확인
+        weight_dict[w] = weight_dict.get(w, 0) + 1
+
+    total_count = 0
+    divisors = (2, 3, 4)
+    b_weight = 0
+
+    for a_weight, num in weight_dict.items():
+                  # 조합의 개수 numC2 계산
+        total_count += num * (num - 1) // 2
+
+        for divisor in divisors:
+            # B의 몸무게가 A몸무게의 50%, 66.6%, 75%일때 중심으로 부터 2~4미터의 거리에 앉아서 균형을 맞출수 있다
+            b_weight = a_weight - a_weight / divisor
+            if b_weight in weight_dict:
+                total_count += weight_dict[b_weight] * num
+
+    return total_count
