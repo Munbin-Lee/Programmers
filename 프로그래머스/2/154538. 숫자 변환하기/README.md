5345076b735eab3c8497653a56e7a64a1ad0a9f8
이문빈 <3412mb@gmail.com>
2023-11-13 02:41:32 +0900

    [level 2] Title: 숫자 변환하기, Time: 101.04 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/README.md
+++ 프로그래머스/2/154538. 숫자 변환하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 변환하기 - 154538 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.27 ms
+메모리: 17.3 MB, 시간: 101.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 2:35:53
+2023년 11월 1일 2:41:32
 
 ### 문제 설명
 
--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,17 +1,28 @@
-from collections import deque
+INT_MAX = 1_000_000
+
 
 def solution(x, y, n):
-    deq = deque([[y, 0]])
-    
-    while deq:
-        cur, cnt = deq.popleft()
-        
-        if cur == x: return cnt
-    
-        if cur < x: continue
-        
-        deq.append([cur - n, cnt + 1])
-        if cur % 2 == 0: deq.append([cur // 2, cnt + 1])
-        if cur % 3 == 0: deq.append([cur // 3, cnt + 1])
-        
-    return -1
+    return min_count(x, y, n)
+
+
+def min_count(x, y, n):
+    vals = [INT_MAX for _ in range(y + 1)]
+    vals[x] = 0
+
+    for i in range(x, y + 1):
+        if vals[i] != INT_MAX:
+            val = vals[i] + 1
+            idx = i + n
+
+            if idx <= y:
+                vals[idx] = min(val, vals[idx])
+
+            idx = i * 2
+            if idx <= y:
+                vals[idx] = min(val, vals[idx])
+
+            idx = i * 3
+            if idx <= y:
+                vals[idx] = min(val, vals[idx])
+
+    return vals[y] if vals[y] != INT_MAX else -1
