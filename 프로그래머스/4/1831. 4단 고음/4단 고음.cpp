e7685eccd73c2d10caabc7abedb82076558c877b
이문빈 <3412mb@gmail.com>
2023-12-12 16:24:48 +0900

    [level 4] Title: 4단 고음, Time: 1796.04 ms, Memory: 6.4 MB -BaekjoonHub

--- 프로그래머스/4/1831. 4단 고음/4단 고음.cpp
+++ 프로그래머스/4/1831. 4단 고음/4단 고음.cpp
@@ -1,26 +1,30 @@
-#include <functional>
+#include <queue>
 #include <cmath>
 
 using namespace std;
+using pr = pair<int, int>;
 
 int solution(int n) {
     int answer = 0;
     
-    function<void(int, int)> dfs = [&](int cur, int plus) {
-        if (pow(3, plus / 2) > cur) return;
+    queue<pr> q;
+    q.emplace(n - 2, 2);
+    
+    while (!q.empty()) {
+        auto [cur, plus] = q.front();
+        q.pop();
         
+        if (pow(3, plus / 2) > cur) continue;
         if (cur == 1) {
             answer += plus == 0;
-            return;
+            continue;
         }
         
         if (cur % 3 == 0 && plus >= 2) {
-            dfs(cur / 3, plus - 2);
+            q.emplace(cur / 3, plus - 2);
         }
-        dfs(cur - 1, plus + 1);
-    };
-    
-    dfs(n - 2, 2);
+        q.emplace(cur - 1, plus + 1);
+    }
     
     return answer;
 }
